58d8715d55bd8c5e2d74d1eac11ea8a6b8980b5d
elsoftpy <develop@elsoft.com.py>
2025-03-29 11:27:10 -0400

    change conceptos

--- resources/views/report/conceptos.blade.php
+++ resources/views/report/conceptos.blade.php
@@ -1,71 +1,125 @@
 @extends('report.layout')
 
 @section('content')
-	@include('report.title')
-	<div class="row">
-		<div class="col s12">
-			<div class="hoverable bordered">
-				<div class="card">
-					<div class="card-content">
-						<h5>BASICO MENSUAL</h5>								
-						<p>Es el salario base bruto estipulado que el colaborador empleado percibe mensualmente, sin incluir otros ingresos adicionales.</p>								
-														
-						<h5>BASICO ANUAL</h5> 								
-						<p>Basico Mensual bruto correspondiente a 12 meses.</p>
-														
-						<h5>EFECTIVO  ANUAL GARANTIZADO</h5>
-						<p>Es el equivalente al Salario Base Anual +  Bonificacion  fija garantizada  (aguinaldo, en el caso de Paraguay)</p>  								
-						<p>Definición: Corresponde salario basico anual  en efectivo que percibe el colaborador  por mantener una posición específica y no depende del resultado del desempeño ya sea de la organización o individual +  la doceava parte del salario base anual (aguinaldo).</p> 																
-						<h5>VARIABLE/ADICIONAL ANUAL </h5>								
-						<p>El concepto VARIABLE corresponde a un concepto remunerativo cuyo determinante es el por desempeño medido a traves de indicadores pre establecidos, con frecuencia por viaje, incluyendo oficiales y subalternos. </p>
-						<p>El concepto ADICIONAL corresponde a un pago fijo  por decision exclusiva de la compañia, para cubrir aspectos que considera necesarios  efectivizar por viaje. El concepto  no esta sujeto exactamente al  desempeño o metas del ocupante del cargo. </p> 								
-														
-						<h5>BONO ANUAL</h5>								
-						<p>Corresponde a la bonificacion otorgada como consecuencia del desempeño y  los resultados de la compañía. Con frecuencia anual.</p> 								
-						<h5>EFECTIVO TOTAL ANUAL</h5>
-						<p>Es el equivalente al Salario Base Anual +  Aguinaldo efectivo  +  Total Adicional Anual + Comision Anual + Bono</p> 								
-						<h5>BENEFICIOS VALORIZADOS - ANUAL</h5>
-						<p>Corresponde a la sumatoria anualizada de todos los conceptos que involucran beneficios asignados para el cargo.</p>
-						<h5>COMPENSACION TOTAL ANUAL</h5> 								
-						<p>Es el equivalente al Salario Base Anual +  Aguinaldo efectivo  +  Total Adicional Anual + Comision Anual + Bono + Beneficios Valorizados</p>								
-						<h5>CONCEPTOS ESTADISTICOS</h5> 								
-														
-						<h5>MINIMO</h5>
-						<p>Es el menor valor de un determinado conjunto de observaciones. En nuestro caso, es la remuneración menor de las observadas para cada cargo.</p>
-														
-						<h5>25 PERCENTIL</h5>
-						<p>Es el valor que separa a una serie de observaciones de forma tal que el 75% de éstas es mayor y el 25% es menor a dicho valor. En términos de la encuesta, significa que el 75% de las remuneraciones otorgadas al cargo encuestado son superiores a este valor.	</p>		
-														
-						<h5>PROMEDIO ARITMETICO </h5>
-						<p>Es un estadígrafo de tendencia central que se obtiene sumando los valores del concepto correspondientes a todas las observaciones de cada cargo y dividiendo el resultado de esta suma entre el número de términos que componen.</p>								
-						
-						<h5>MEDIANA</h5>
-						<p>Es el valor que separa al conjunto de observaciones en forma tal que el 50% de éstas sea mayor y el 50% es menor a dicho valor.</p>
-						<h5>75 PERCENTIL</h5>								
-						<p>Es el valor que separa al conjunto de observaciones en forma tal que el 25% de éstas es mayor y el 75% es menor a dicho valor. En términos de la encuesta, significa que el 75% de las remuneraciones otorgadas al cargo encuestado es inferior a dicho valor.</p>		
-														
-						<h5>MAXIMO</h5>								
-						<p>Es el mayor valor del conjunto de observaciones. En nuestro caso, es la remuneración mayor de las observadas para cada cargo.</p>								
-					</div>
-				</div>
+    @include('report.title')
+    <div class="row">
+        <div class="col s12">
+            <div class="hoverable bordered">
+                <div class="card">
+                    <div class="card-content">
+                        <h5><span>BASICO MENSUAL</span></h5>
 
-			</div>
-		</div>
-	</div>
+                        <p><span>Es el salario base bruto estipulado que el colaborador empleado percibe mensualmente, sin
+                                incluir otros ingresos adicionales.</span></p>
+
+                        <h5><span>BASICO ANUAL</span></h5>
+
+                        <p><span>Básico Mensual Bruto correspondiente a 12 meses calendario. </span></p>
+
+                        <h5><span>EFECTIVO ANUAL GARANTIZADO</span></h5>
+
+                        <p><span>Es el equivalente al Salario Base Anual + Bonificación Fija garantizada (ejemplo
+                                gratificación fija, bono de fin de año o bien aguinaldo)</span></p>
+						<br>
+                        <p><span style="font-weight: bold;">Salario Base Anual: </span><span>corresponde al salario básico anual en efectivo que percibe el colaborador por mantener
+                                una posición específica. </span></p>
+						<br>								
+                        <p><span style="font-weight: bold;">Remuneración anual complementaria o aguinaldo: </span><span>equivalente a la doceava parte de las remuneraciones devengadas durante el año calendario
+                                a favor del colaborador. </span></p>
+
+                        <h5><span>VARIABLE</span></h5>
+
+                        <p><span>El salario variable es la parte de la remuneración de un colaborador que varía en función
+                                de su desempeño. Se trata de un incentivo que se paga en reconocimiento a la contribución
+                                del empleado.</span></p>
+
+                        <h5><span>VARIABLE ANUAL</span></h5>
+
+                        <p><span>Es el Salario Variable percibido durante un año calendario. </span></p>
+
+                        <h5><span>ADICIONAL</span></h5>
+
+                        <p><span>Es la remuneración extra que se paga a un colaborador para complementar su salario base
+                                (complemento salarial). </span></p>
+
+                        <h5><span>ADICIONAL ANUAL</span></h5>
+
+                        <p><span>Es el Adicional percibido durante un año calendario. </span></p>
+
+                        <h5><span>ADICIONAL TOTAL</span></h5>
+
+                        <p><span>Variable Anual + Adicional Anual </span></p>
+
+                        <h5><span>BONO</span></h5>
+
+                        <p><span>Es un pago adicional al salario regular de un colaborador. </span></p>
+
+                        <h5><span>EFECTIVO TOTAL ANUAL</span></h5>
+
+                        <p><span>Salario Base Anual + Aguinaldo + Total Adicional Anual + Bono Anual</span></p>
+
+                        <h5><span>BENEFICIOS VALORIZADOS - ANUAL</span></h5>
+
+                        <p><span>Corresponde a la sumatoria anualizada de todos los conceptos que involucran beneficios
+                                asignados para el cargo.</span></p>
+
+                        <h5><span>COMPENSACION TOTAL ANUAL</span></h5>
+
+                        <p><span>Es el equivalente al Salario Base Anual + Aguinaldo Efectivo + Total Adicional Anual + Bono
+                                + Beneficios Valorizados</span></p>
+
+                        <h5><span>CONCEPTOS ESTADISTICOS</span></h5>
+
+                        <p><span>MINIMO</span></p>
+
+                        <p><span>Es el menor valor de un determinado conjunto de observaciones. En nuestro caso, es la
+                                remuneración menor de las observadas para cada cargo.</span></p>
+						<br>
+                        <p><span>25 PERCENTIL</span></p>
+						
+                        <p><span>Es el valor que separa a una serie de observaciones de forma tal que el 75% de éstas es
+                                mayor y el 25% es menor a dicho valor. En términos de la encuesta, significa que el 75% de
+                                las remuneraciones otorgadas al cargo encuestado son superiores a este valor.</span></p>
+						<br>
+                        <p><span>PROMEDIO ARITMETICO</span></p>
+						
+                        <p><span>Es un estadígrafo de tendencia central que se obtiene sumando los valores del concepto
+                                correspondientes a todas las observaciones de cada cargo y dividiendo el resultado de esta
+                                suma entre el número de términos que componen.</span></p>
+						<br>
+                        <p><span>MEDIANA</span></p>
+
+                        <p><span>Es el valor que separa al conjunto de observaciones en forma tal que el 50% de éstas sea
+                                mayor y el 50% es menor a dicho valor.</span></p>
+						<br>	
+                        <p><span>75 PERCENTIL</span></p>
+
+                        <p><span>Es el valor que separa al conjunto de observaciones en forma tal que el 25% de éstas es
+                                mayor y el 75% es menor a dicho valor. En términos de la encuesta, significa que el 75% de
+                                las remuneraciones otorgadas al cargo encuestado es inferior a dicho valor.</span></p>
+						<br>
+                        <p><span>MAXIMO</span></p>
+
+                        <p><span>Es el mayor valor del conjunto de observaciones. En nuestro caso, es la remuneración mayor
+                                de las observadas para cada cargo.</span></p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @stop
 @push('scripts')
-	<script type="text/javascript">
-		if (RegExp('multipage=true', 'gi').test(window.location.search)) {
-		    tour.goToStepNumber(26).start();
-	    }
+    <script type="text/javascript">
+        if (RegExp('multipage=true', 'gi').test(window.location.search)) {
+            tour.goToStepNumber(26).start();
+        }
 
-     	tour.onafterchange(function(step){
-			if($(step).attr("data-step") == 27){
-				window.location.href="{{URL::route('reportes.panel', $dbEmpresa)}}?multipage=true";
-			}
-      	});    		    	    
-
-
-
-	</script>
+        tour.onafterchange(function(step) {
+            if ($(step).attr("data-step") == 27) {
+                window.location.href = "{{ URL::route('reportes.panel', $dbEmpresa) }}?multipage=true";
+            }
+        });
+    </script>
 @endpush
--- resources/views/report/conceptos_en.blade.php
+++ resources/views/report/conceptos_en.blade.php
@@ -1,13 +1,13 @@
 @extends('report.layout')
 
 @section('content')
-	@include('report.title')
-	<div class="row">
-		<div class="col s12">
-			<div class="hoverable bordered">
-				<div class="card">
-					<div class="card-content">
-						<h5>STATISTICAL CONCEPTS</h5>								
+    @include('report.title')
+    <div class="row">
+        <div class="col s12">
+            <div class="hoverable bordered">
+                <div class="card">
+                    <div class="card-content">
+                        {{-- <h5>STATISTICAL CONCEPTS</h5>								
 						<div class="conceptos_estadisticos">
 							<p><strong>Minimum:</strong> minimum value of a list of the series.</p> 
 							<p><strong>Quartile:</strong> the first quartile of a numerically ordered list is a number such that a quarter of the data in the list is below it.</p>
@@ -15,26 +15,119 @@
 							<p><strong>2nd Quartile or Median:</strong> number that divides a group of numerically ordered data into a lower and a higher half. The second quartile is the same as the median.</p>
 							<p><strong>Mean (average):</strong> a mean score is an average score. It is the sum of individual scores divided by the number of individuals.</p> 
 							<p><strong>Maximum:</strong> the maximum value of a list of the series
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+						</div> --}}
+                        <h5><span>MONTHLY BASIC</span></h5>
+
+                        <p><span>It is the stipulated gross base salary that the employee receives monthly, without
+                                including other additional income.</span></p>
+
+                        <h5><span>ANNUAL BASIC</span></h5>
+
+                        <p><span>Gross Monthly Basic corresponding to 12 calendar months.</span></p>
+
+                        <h5><span>GUARANTEED ANNUAL CASH</span></h5>
+
+                        <p><span>It is equivalent to the Annual Base Salary + Guaranteed Fixed Bonus (e.g., fixed gratuity,
+                                year-end bonus, or Christmas bonus).</span></p>
+						<br>
+                        <p><span style="font-weight:bold;">Annual Base Salary: </span><span> corresponds to the annual cash basic salary that the employee receives for holding a
+                                specific position.</span></p>
+						<br>
+                        <p><span style="font-weight:bold;">Complementary Annual Compensation or Christmas Bonus:</span>
+							<span> equivalent to one-twelfth of the remuneration accrued during the calendar year in favor
+                                of the employee.</span></p>
+                        <h5><span>VARIABLE</span></h5>
+
+                        <p><span>Variable salary is the part of an employee's compensation that varies based on their
+                                performance. It is an incentive paid in recognition of the employee's contribution.</span>
+                        </p>
+
+                        <h5><span>ANNUAL VARIABLE</span></h5>
+
+                        <p><span>It is the Variable Salary received during a calendar year.</span></p>
+
+                        <h5><span>ADDITIONAL</span></h5>
+
+                        <p><span>It is the extra remuneration paid to an employee to complement their base salary (salary
+                                supplement).</span></p>
+
+                        <h5><span>ANNUAL ADDITIONAL</span></h5>
+
+                        <p><span>It is the Additional received during a calendar year.</span></p>
+
+                        <h5><span>TOTAL ADDITIONAL</span></h5>
+
+                        <p><span>Annual Variable + Annual Additional</span></p>
+
+                        <h5><span>BONUS</span></h5>
+
+                        <p><span>It is an additional payment to an employee's regular salary.</span></p>
+
+                        <h5><span>TOTAL ANNUAL CASH</span></h5>
+
+                        <p><span>Annual Base Salary + Christmas Bonus + Total Annual Additional + Annual Bonus</span></p>
+
+                        <h5><span>VALUED BENEFITS - ANNUAL</span></h5>
+
+                        <p><span>Corresponds to the annualized sum of all concepts involving benefits assigned to the
+                                position.</span></p>
+
+                        <h5><span>TOTAL ANNUAL COMPENSATION</span></h5>
+
+                        <p><span>It is equivalent to the Annual Base Salary + Effective Christmas Bonus + Total Annual
+                                Additional + Bonus + Valued Benefits.</span></p>
+
+                        <h5><span>STATISTICAL CONCEPTS</span></h5>
+
+                        <p><span>MINIMUM</span></p>
+
+                        <p><span>It is the lowest value in a given set of observations. In our case, it is the lowest
+                                remuneration observed for each position.</span></p>
+						<br>
+                        <p><span>25TH PERCENTILE</span></p>
+
+                        <p><span>It is the value that separates a series of observations so that 75% are higher and 25% are
+                                lower than that value. In survey terms, it means that 75% of the remunerations granted for
+                                the surveyed position are higher than this value.</span></p>
+						<br>
+                        <p><span>ARITHMETIC AVERAGE</span></p>
+
+                        <p><span>It is a measure of central tendency obtained by summing the values of the concept
+                                corresponding to all observations of each position and dividing the result of this sum by
+                                the number of terms that compose it.</span></p>
+						<br>
+                        <p><span>MEDIAN</span></p>
+
+                        <p><span>It is the value that separates the set of observations so that 50% are higher and 50% are
+                                lower than that value.</span></p>
+						<br>
+                        <p><span>75TH PERCENTILE</span></p>
+
+                        <p><span>It is the value that separates the set of observations so that 25% are higher and 75% are
+                                lower than that value. In survey terms, it means that 75% of the remunerations granted for
+                                the surveyed position are lower than this value.</span></p>
+						<br>
+                        <p><span>MAXIMUM</span></p>
+
+                        <p><span>It is the highest value in the set of observations. In our case, it is the highest
+                                remuneration observed for each position.</span></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 @push('scripts')
-	<script type="text/javascript">
-		if (RegExp('multipage=true', 'gi').test(window.location.search)) {
-		    tour.goToStepNumber(26).start();
-	    }
+    <script type="text/javascript">
+        if (RegExp('multipage=true', 'gi').test(window.location.search)) {
+            tour.goToStepNumber(26).start();
+        }
 
-     	tour.onafterchange(function(step){
-			if($(step).attr("data-step") == 27){
-				window.location.href="{{URL::route('reportes.panel', $dbEmpresa)}}?multipage=true";
-			}
-      	});    		    	    
-
-
-
-	</script>
+        tour.onafterchange(function(step) {
+            if ($(step).attr("data-step") == 27) {
+                window.location.href = "{{ URL::route('reportes.panel', $dbEmpresa) }}?multipage=true";
+            }
+        });
+    </script>
 @endpush
